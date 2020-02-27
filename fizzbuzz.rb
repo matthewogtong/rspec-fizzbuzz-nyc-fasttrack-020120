d1f482b7e6093283df0ee90ab4e2ff53fd64a84b
Matthew Ogtong <ogtongm@gmail.com>
2020-02-27 17:07:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 & 5 == 0
-    "Fizz"
-  elsif int % 3 == 0
-    "Buzz"
-  elsif int % 5 == 0
     "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
   end
 end
